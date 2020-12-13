68463ed4db024ef0b8effb7da61940ebc3e1865f
GermanShtey <gvshteynardt@gmail.com>
2020-12-13 15:09:48 +0300

    feat: создать модель user & article

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-const { PORT = 3000 } = process.env;
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/newsdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.listen(PORT, () => {
   console.log(`server is running ${PORT}`);
---
+++ models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const isEmail = require('validator/lib/isEmail');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, `поле 'email' должно быть заполнено`],
+    unique: [true, 'email must be unique'],
+    validate: {
+      validator: (value) => isEmail(value),
+      message: `поле 'email' должно быть валидным email адресом`,
+    },
+  },
+  password: {
+    type: String,
+    required: [true, `поле 'password' должно быть заполнено`],
+    minlength: [ 5, `минимальная длина поля 'password' - 5`],
+    select: false,
+  },
+  name: {
+    type: String,
+    required: false,
+    minlength: [ 2, `минимальная длина поля 'name' - 2`],
+    maxlength: [ 30, `максимальная длина поля 'name' - 5`],
+  },
+});
+
+userSchema.statics.findUsersByCredentials = async function (email, password) {
+  const user = await this.findOne({ email }).select('+password');
+  if (!user) {
+    //throw new UnauthorizedErr('Неправильные почта или пароль');
+  }
+
+  const matched = await bcrypt.compare(password, user.password);
+  if (!matched) {
+    //throw new UnauthorizedErr('Неправильные почта или пароль');
+  }
+  return user;
+};
+
+module.exports = mongoose.model('user', userSchema);
